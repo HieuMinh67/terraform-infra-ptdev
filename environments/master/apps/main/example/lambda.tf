5406d3a706dcd007e49cce9198eee0be20f81b45
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-06 01:36:23 +0700

    test_world_lambda

--- environments/master/apps/main/example/lambda.tf
+++ environments/master/apps/main/example/lambda.tf
@@ -211,7 +211,7 @@ resource "aws_api_gateway_integration" "lambda" {
 
    integration_http_method = "POST"
    type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.test_lambda.invoke_arn
+   uri                     = aws_lambda_function.test_world_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
@@ -228,7 +228,7 @@ resource "aws_api_gateway_integration" "lambda_root" {
 
    integration_http_method = "POST"
    type                    = "AWS_PROXY"
-   uri                     = aws_lambda_function.test_lambda.invoke_arn
+   uri                     = aws_lambda_function.test_world_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "example" {
@@ -244,7 +244,7 @@ resource "aws_api_gateway_deployment" "example" {
 resource "aws_lambda_permission" "apigw" {
    statement_id  = "AllowAPIGatewayInvoke"
    action        = "lambda:InvokeFunction"
-   function_name = aws_lambda_function.test_lambda.function_name
+   function_name = aws_lambda_function.test_world_lambda.function_name
    principal     = "apigateway.amazonaws.com"
 
    # The "/*/*" portion grants access from any method on any resource

9bb22dc66c53ba03132b2af9371210aaab0088f1
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-05 21:34:32 +0700

    Update lambda.tf

--- environments/master/apps/main/example/lambda.tf
+++ environments/master/apps/main/example/lambda.tf
@@ -209,10 +209,39 @@ resource "aws_api_gateway_integration" "lambda" {
    resource_id = aws_api_gateway_method.proxy.resource_id
    http_method = aws_api_gateway_method.proxy.http_method
 
-   integration_http_method = "GET"
+   integration_http_method = "POST"
    type                    = "AWS_PROXY"
    uri                     = aws_lambda_function.test_lambda.invoke_arn
 }
+
+resource "aws_api_gateway_method" "proxy_root" {
+   rest_api_id   = aws_api_gateway_rest_api.example.id
+   resource_id   = aws_api_gateway_rest_api.example.root_resource_id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+   rest_api_id = aws_api_gateway_rest_api.example.id
+   resource_id = aws_api_gateway_method.proxy_root.resource_id
+   http_method = aws_api_gateway_method.proxy_root.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.test_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "example" {
+   depends_on = [
+     aws_api_gateway_integration.lambda,
+     aws_api_gateway_integration.lambda_root,
+   ]
+
+   rest_api_id = aws_api_gateway_rest_api.example.id
+   stage_name  = "test"
+}
+
+
 
 
 # This is to optionally manage the CloudWatch Log Group for the Lambda Function.

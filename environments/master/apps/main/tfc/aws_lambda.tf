658278d3134061ee988f5d3b7aa6cf9b4615482e
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-13 00:45:55 +0700

    Update aws_lambda.tf

--- environments/master/apps/main/tfc/aws_lambda.tf
+++ environments/master/apps/main/tfc/aws_lambda.tf
@@ -103,3 +103,83 @@ data "aws_s3_bucket_object" "notification" {
 }
 
 
+resource "aws_lambda_function" "test_wo_lambda" {
+  s3_bucket     = "479284709538-${var.aws_region}-aws-lambda"
+  s3_key        = "terraform-api/latest/world.zip"
+  function_name = "worldtfc"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "world"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = data.aws_s3_bucket_object.mylambdacode_world.etag
+
+  runtime = "provided"
+
+  environment {
+    variables = {
+      API_KEY = "api_key_bar"
+    }
+  }
+
+
+  file_system_config {
+    # EFS file system access point ARN
+    arn = aws_efs_access_point.access_point_for_lambda.arn
+
+    # Local mount path inside the lambda function. Must start with '/mnt/'.
+    local_mount_path = "/mnt/efs"
+  }
+
+  vpc_config {
+    # Every subnet should be able to reach an EFS mount target in the same Availability Zone. Cross-AZ mounts are not permitted.
+    subnet_ids         = [aws_subnet.subnet_for_lambda.id]
+    security_group_ids = [aws_security_group.sg_for_lambda.id]
+  }
+
+  # Explicitly declare dependency on EFS mount target.
+  # When creating or updating Lambda functions, mount target must be in 'available' lifecycle state.
+  depends_on = [
+    aws_efs_mount_target.bean,
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.bean-notification
+  ]
+}
+
+
+# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
+# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
+resource "aws_cloudwatch_log_group" "bean-notification" {
+  name              = "/aws/lambda/bean-notification"
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}

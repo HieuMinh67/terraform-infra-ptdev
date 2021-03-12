70257ea673a683d29181862da896197d905b682e
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-12 22:53:48 +0700

    Update aws_lambda.tf

--- environments/master/apps/main/tfc/aws_lambda.tf
+++ environments/master/apps/main/tfc/aws_lambda.tf
@@ -4,13 +4,6 @@
 */
   
 # https://learn.hashicorp.com/tutorials/terraform/lambda-api-gateway?in=terraform/aws
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
 
 provider "aws" {
    region = var.aws_region

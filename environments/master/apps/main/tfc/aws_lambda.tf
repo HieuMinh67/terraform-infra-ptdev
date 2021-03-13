58fbd39cefd8a951cfb54511f72a1e0d4ec4ce2a
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-13 11:33:10 +0700

    Update aws_lambda.tf

--- environments/master/apps/main/tfc/aws_lambda.tf
+++ environments/master/apps/main/tfc/aws_lambda.tf
@@ -105,12 +105,12 @@ data "aws_s3_bucket_object" "notification" {
 }
 
 
-resource "aws_lambda_function" "test_wo_lambda" {
+resource "aws_lambda_function" "notification" {
   s3_bucket     = "479284709538-${var.aws_region}-aws-lambda"
-  s3_key        = "terraform-api/latest/world.zip"
-  function_name = "worldtfc"
+  s3_key        = "terraform-api/latest/notification.zip"
+  function_name = "notification"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "world"
+  handler       = "notification"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:

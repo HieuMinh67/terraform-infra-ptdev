bc6cd56a877a22ecca3d0338a30883a574774e05
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-13 23:56:42 +0700

    Update aws_api_gateway.tf

--- environments/master/apps/main/tfc/aws_api_gateway.tf
+++ environments/master/apps/main/tfc/aws_api_gateway.tf
@@ -16,14 +16,14 @@ The "REST API" is the container for all of the other API Gateway objects we will
 All incoming requests to API Gateway must match with a configured resource and method in order to be handled. 
 Append the following to define a single proxy resource:
 */
-resource "aws_api_gateway_resource" "bean-proxy" {
+resource "aws_api_gateway_resource" "bean-notification-proxy" {
    rest_api_id = aws_api_gateway_rest_api.bean-notification.id
    parent_id   = aws_api_gateway_rest_api.bean-notification.root_resource_id
    path_part   = "{proxy+}"
 }
-resource "aws_api_gateway_method" "bean-proxy" {
+resource "aws_api_gateway_method" "bean-notification-proxy" {
    rest_api_id   = aws_api_gateway_rest_api.bean-notification.id
-   resource_id   = aws_api_gateway_resource.bean-proxy.id
+   resource_id   = aws_api_gateway_resource.bean-notification-proxy.id
    http_method   = "ANY"
    authorization = "NONE"
 }
@@ -39,6 +39,29 @@ resource "aws_api_gateway_integration" "bean-notification" {
    rest_api_id = aws_api_gateway_rest_api.bean-notification.id
    resource_id = aws_api_gateway_method.bean-proxy.resource_id
    http_method = aws_api_gateway_method.bean-proxy.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.bean-notification.invoke_arn
+}
+
+/*
+The AWS_PROXY integration type causes API gateway to call into the API of another AWS service. 
+In this case, it will call the AWS Lambda API to create an "invocation" of the Lambda function.
+Unfortunately the proxy resource cannot match an empty path at the root of the API. 
+To handle that, a similar configuration must be applied to the root resource that is built in to the REST API object:
+*/
+resource "aws_api_gateway_method" "bean-notification-proxy_root" {
+   rest_api_id   = aws_api_gateway_rest_api.example.id
+   resource_id   = aws_api_gateway_rest_api.example.root_resource_id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "bean-notification_root" {
+   rest_api_id = aws_api_gateway_rest_api.bean-notification.id
+   resource_id = aws_api_gateway_method.bean-notification-proxy_root.resource_id
+   http_method = aws_api_gateway_method.bean-notification-proxy_root.http_method
 
    integration_http_method = "POST"
    type                    = "AWS_PROXY"

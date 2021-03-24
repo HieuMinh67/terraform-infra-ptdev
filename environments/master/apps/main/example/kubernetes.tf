a57450af5daaec33698a237f79152a1f57732d79
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-24 16:40:46 +0700

    Update kubernetes.tf

--- environments/master/apps/main/example/kubernetes.tf
+++ environments/master/apps/main/example/kubernetes.tf
@@ -162,13 +162,6 @@ module "eks" {
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t3.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
   ]
 
   worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]

dcc1bef721cf22fa131cfc690d3d069f69df19ab
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-24 18:29:52 +0700

    Update kubernetes.tf

--- environments/master/apps/main/example/kubernetes.tf
+++ environments/master/apps/main/example/kubernetes.tf
@@ -199,14 +199,16 @@ output "kubeconfig_path" {
 }
   
   
-  
-  
-  
-  
+  resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+MPbSoDnncbAAFGw0aammN5bimYrQ1533jyiYr3fTa8aN7U1CEbKBdnK1KUSubEVMtTyVGqcJ5EWoZo0sNkdFcjauhtuoojD7J6TKq6ML/YzUbNpTZo0l4Zq/N2PG/jM3Nyj4y6j2YKAkQK6FSofwY41Z+uiifTEcico2mSRuqwxSucEPGVZZ4x0mR2CbNnV5oTEF+KTktqVx+pw8d3wMtQWAu4f/HgeoQudXLNsyjzhb57JxpXKDVj+tNVKt5W9xBdSZqe2wb7f5xSay95sIJXZK6xUhUaVe57kxR2QiXUfapAIG4B2nheHdaFhINDrq01s8JZCCoTFzOHMXuEId centos@moodle.pnt.edu.vn"
+}
+
+ 
   module "bastion" {
   source            = "github.com/jetbrains-infra/terraform-aws-bastion-host"  
   subnet_id         = module.vpc.public_subnets[0]
-  ssh_key           = "ssh_key_name"
+  ssh_key           = "deployer-key"
   internal_networks = ["10.0.0.0/16"]
   disk_size         = 10
   instance_type     = "t2.micro"

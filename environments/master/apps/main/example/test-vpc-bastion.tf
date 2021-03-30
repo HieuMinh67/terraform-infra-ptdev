146df3204df0ef66052b1b632737c5c92230da09
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-30 22:52:11 +0700

    Update test-vpc-bastion.tf

--- environments/master/apps/main/example/test-vpc-bastion.tf
+++ environments/master/apps/main/example/test-vpc-bastion.tf
@@ -41,22 +41,6 @@ provider "template" {
   version = "~> 2.1"
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.11"
-}
-
 data "aws_availability_zones" "available" {
 }
 
@@ -167,21 +151,6 @@ module "vpc" {
   }
 }
     
-    output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
-}
-
-output "config_map_aws_auth" {
-  description = "A kubernetes configuration to authenticate to this EKS cluster."
-  value       = module.eks.config_map_aws_auth
-}
-
 output "region" {
   description = "AWS region."
   value       = var.region

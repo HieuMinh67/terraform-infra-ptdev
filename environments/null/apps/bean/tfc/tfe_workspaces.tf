268c2d81d28e78b85fe1bb1897e6914749f47c1a
Peter Bean <binh@sunrise.vn>
2021-03-31 22:50:35 +0700

    Fix VPC Name

--- environments/null/apps/bean/tfc/tfe_workspaces.tf
+++ environments/null/apps/bean/tfc/tfe_workspaces.tf
@@ -3,7 +3,7 @@ provider "tfe" {
   token = var.tfe_token
 }
 
-data "tfe_workspace" "bean-tfc" {
+data "tfe_workspace" "this-tfc" {
   name         = "${var.environment}-terraform-cloud"
   organization = "BeanTraining"
 }
@@ -61,7 +61,7 @@ variable "environment" {
   default = "dev-sg"
 }
 
-resource "tfe_oauth_client" "bean-github" {
+resource "tfe_oauth_client" "this-github" {
   organization     = "BeanTraining"
   api_url          = "https://api.github.com"
   http_url         = "https://github.com"
@@ -69,7 +69,7 @@ resource "tfe_oauth_client" "bean-github" {
   service_provider = "github"
 }
 
-resource "tfe_workspace" "bean" {
+resource "tfe_workspace" "this" {
   for_each            = { for ws in var.workspaces : "${var.environment}-${ws.app_type}-${ws.app_category}-${ws.app_name}" => ws }
   name                = "${var.environment}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}"
   organization        = "BeanTraining"
@@ -90,15 +90,15 @@ resource "tfe_workspace" "bean" {
   auto_apply = each.value.auto_apply
 }
 
-resource "tfe_run_trigger" "bean" {
+resource "tfe_run_trigger" "this" {
   for_each      = { for ws in var.workspaces : "${var.environment}-${ws.app_type}-${ws.app_category}-${ws.app_name}" => ws }
   workspace_id  = tfe_workspace.this["${var.environment}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}"].id
   sourceable_id = each.value.depends_on == "" ? data.tfe_workspace.this-tfc.id : tfe_workspace.this["${var.environment}-${each.value.depends_on}"].id
 }
   
-resource "tfe_notification_configuration" "bean-auto-approver" {
-  for_each         = { for ws in var.workspaces : "bean-auto-approver-${var.environment}-${ws.app_type}-${ws.app_category}-${ws.app_name}" => ws }
-  name             = "bean-auto-approver-${var.environment}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}"
+resource "tfe_notification_configuration" "this-auto-approver" {
+  for_each         = { for ws in var.workspaces : "this-auto-approver-${var.environment}-${ws.app_type}-${ws.app_category}-${ws.app_name}" => ws }
+  name             = "this-auto-approver-${var.environment}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}"
   enabled          = true
   destination_type = "generic"
   triggers         = ["run:needs_attention"]

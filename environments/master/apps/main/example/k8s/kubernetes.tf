7bab62f811e5f53d0e0a9bd3229f6d1479723d7f
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-03-31 16:04:21 +0700

    Update kubernetes.tf

--- environments/master/apps/main/example/k8s/kubernetes.tf
+++ environments/master/apps/main/example/k8s/kubernetes.tf
@@ -213,11 +213,7 @@ output "region" {
   output "cluster_name" {
     value = local.cluster_name
     }
-  
-  resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+MPbSoDnncbAAFGw0aammN5bimYrQ1533jyiYr3fTa8aN7U1CEbKBdnK1KUSubEVMtTyVGqcJ5EWoZo0sNkdFcjauhtuoojD7J6TKq6ML/YzUbNpTZo0l4Zq/N2PG/jM3Nyj4y6j2YKAkQK6FSofwY41Z+uiifTEcico2mSRuqwxSucEPGVZZ4x0mR2CbNnV5oTEF+KTktqVx+pw8d3wMtQWAu4f/HgeoQudXLNsyjzhb57JxpXKDVj+tNVKt5W9xBdSZqe2wb7f5xSay95sIJXZK6xUhUaVe57kxR2QiXUfapAIG4B2nheHdaFhINDrq01s8JZCCoTFzOHMXuEId centos@moodle.pnt.edu.vn"
-}
+ 
 
   variable "region" {
   default = "us-west-2"

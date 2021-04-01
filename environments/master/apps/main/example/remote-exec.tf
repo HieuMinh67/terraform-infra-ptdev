221091f8aed6197c227f6c0f75f7c07f84fd5785
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-04-01 07:06:40 +0700

    Update remote-exec.tf

--- environments/master/apps/main/example/remote-exec.tf
+++ environments/master/apps/main/example/remote-exec.tf
@@ -78,6 +78,15 @@ resource "null_resource" "example_provisioner" {
       
       "chmod +x ~/terraform-init.sh",
       "~/terraform-init.sh '${var.github_oauth_token}' '${var.tfe_token}' '${var.aws_access_key_id}' '${var.aws_secret_access_key}' '${var.aws_region}' > ~/terraform-init.log",
+      "terraform apply -auto-approve"
+    ]
+  }
+
+      provisioner "remote-exec" {
+        when = "destroy"
+    inline = [
+      "cd environments/master/apps/main/example/k8s",
+      "terraform destroy -auto-approve"
     ]
   }
 

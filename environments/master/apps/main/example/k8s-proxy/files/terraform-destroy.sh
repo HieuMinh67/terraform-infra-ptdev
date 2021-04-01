438c54c3504f2e0372d2bcb506bded5b09d98932
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-04-02 00:06:32 +0700

    Update terraform-destroy.sh

--- environments/master/apps/main/example/k8s-proxy/files/terraform-destroy.sh
+++ environments/master/apps/main/example/k8s-proxy/files/terraform-destroy.sh
@@ -1,3 +1,3 @@
 cd terraform-infra/environments/master/apps/main/example/k8s
-terraform state rm module.$1.module.$2.kubernetes_config_map.aws_auth[0]
+terraform state rm module.$1.kubernetes_config_map.aws_auth[0]
 terraform destroy -auto-approve

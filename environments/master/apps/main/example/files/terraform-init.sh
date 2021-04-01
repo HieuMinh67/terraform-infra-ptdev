8d219728d1f28a9986a10355fdc6647ce76671f6
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-04-01 07:43:59 +0700

    Update terraform-init.sh

--- environments/master/apps/main/example/files/terraform-init.sh
+++ environments/master/apps/main/example/files/terraform-init.sh
@@ -7,4 +7,6 @@ aws --profile default configure set aws_access_key_id "$3"
 aws --profile default configure set aws_secret_access_key "$4"
 aws --profile default configure set region "$5"
 aws --profile default configure set output "json"
+printf "aws_access_key_id = \"$3\" " > aws.auto.tfvars
+print "aws_secret_access_key = \"$4\" " >> aws.auto.tfvars
 # aws eks --region $(terraform output -raw region) update-kubeconfig --name $(terraform output -raw cluster_name)

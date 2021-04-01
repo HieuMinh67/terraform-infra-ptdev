b4c0d0dd2d050759103697e62d1daecb1a05e093
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-04-01 23:37:05 +0700

    Update kubernetes.tf

--- environments/master/apps/main/example/k8s/kubernetes.tf
+++ environments/master/apps/main/example/k8s/kubernetes.tf
@@ -61,7 +61,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "test-eks-${random_string.suffix.result}"
+  cluster_name = "test_cluster" # -eks-${random_string.suffix.result}
 }
 
 resource "random_string" "suffix" {

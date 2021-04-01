553e60c42d7584fdb29cea22626630fe4849f99a
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-04-01 09:46:01 +0700

    Update terraform-apply.sh

--- environments/master/apps/main/example/files/terraform-apply.sh
+++ environments/master/apps/main/example/files/terraform-apply.sh
@@ -1,1 +1,1 @@
-cd terraform-infra/environments/master/apps/main/example/k8s && terraform apply -auto-approve > ~/terraform-apply.log
+cd terraform-infra/environments/master/apps/main/example/k8s && terraform apply -auto-approve

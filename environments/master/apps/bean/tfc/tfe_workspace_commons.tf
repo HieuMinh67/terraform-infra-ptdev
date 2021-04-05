c5fb65d84c07c2979dcf8a315d8a58800655edae
Peter Bean <binh@sunrise.vn>
2021-04-05 20:32:04 +0700

    Symlinks pre-des->des and pre-master -> master

--- environments/master/apps/bean/tfc/tfe_workspace_commons.tf
+++ environments/master/apps/bean/tfc/tfe_workspace_commons.tf
@@ -4,6 +4,17 @@ data "tfe_workspace" "this-tfc" {
   name         = "${var.environment}-terraform-cloud"
   organization = "BeanTraining"
 }
+variable "workspaces" {
+  type = list(object({
+    base_directory   = string
+    app_type         = string
+    app_category     = string
+    app_name         = string
+    auto_apply       = bool
+    depends_on       = string
+    trigger_prefixes = list(string)
+  }))
+}
 
 variable "notification_endpoint" {
   type = string
@@ -26,6 +37,27 @@ resource "tfe_oauth_client" "this-github" {
   http_url         = "https://github.com"
   oauth_token      = var.github_oauth_token
   service_provider = "github"
+}
+
+resource "tfe_workspace" "this" {
+  for_each            = { for ws in var.workspaces : "${var.environment}-${ws.app_type}-${ws.app_category}-${ws.app_name}" => ws }
+  name                = "${var.environment}-${each.value.app_type}-${each.value.app_category}-${each.value.app_name}"
+  organization        = "BeanTraining"
+  speculative_enabled = true
+  queue_all_runs      = each.value.depends_on == "" ? true : false
+  working_directory   = local.working_directory
+  trigger_prefixes = concat(each.value.trigger_prefixes,
+    [
+      local.working_directory,
+      "${each.value.base_directory}/apps/bean/tfc/releases"
+    ]
+  )
+  vcs_repo {
+    identifier     = "BeanTraining/terraform-infra"
+    branch         = var.environment
+    oauth_token_id = tfe_oauth_client.this-github.oauth_token_id
+  }
+  auto_apply = each.value.auto_apply
 }
 
 resource "tfe_run_trigger" "this" {

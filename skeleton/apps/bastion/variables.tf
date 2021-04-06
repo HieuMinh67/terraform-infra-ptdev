ec2dd8e19a96c2bcec212b85f4890ea8b607d4e4
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-04-06 10:20:45 +0700

    Update variables.tf

--- skeleton/apps/bastion/variables.tf
+++ skeleton/apps/bastion/variables.tf
@@ -22,6 +22,7 @@ variable "private_key" {
   }
 variable "github_oauth_token" { }
 variable "tfe_token" {}
+variable "environment" {}
 variable "aws_secret_access_key" {  }
 variable "aws_access_key_id" {  }
 variable "aws_region" {  }

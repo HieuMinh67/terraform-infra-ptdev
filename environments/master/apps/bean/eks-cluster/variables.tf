66bb8f3625e6c00c2b860a57baf671a24a7b9a28
Peter Bean <binh@sunrise.vn>
2021-04-07 00:13:11 +0700

    Fix vars

--- environments/master/apps/bean/eks-cluster/variables.tf
+++ environments/master/apps/bean/eks-cluster/variables.tf
@@ -7,6 +7,12 @@ variable "app_category" {}
 variable "app_name" {}
 variable "environment" {}
 variable "organisation" {}
+variable "tfe_token" {
+  default = "NOT_REQUIRED"
+}
+variable "github_oauth_token" {
+  default = "NOT_REQUIRED"
+}
 
 locals {
     app_name = "${var.environment}-${var.app_type}-${var.app_category}-${var.app_name}"

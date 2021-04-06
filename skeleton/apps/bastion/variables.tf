5148026a2c885a6b9017e236982a0778afec3d4c
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-04-06 10:23:00 +0700

    Update variables.tf

--- skeleton/apps/bastion/variables.tf
+++ skeleton/apps/bastion/variables.tf
@@ -23,6 +23,7 @@ variable "private_key" {
 variable "github_oauth_token" { }
 variable "tfe_token" {}
 variable "environment" {}
+variable "organisation" {}
 variable "aws_secret_access_key" {  }
 variable "aws_access_key_id" {  }
 variable "aws_region" {  }

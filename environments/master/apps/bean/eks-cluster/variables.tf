3ead4b6ed3bebf1acad480f0fe5ed78c125023b3
Peter Bean <binh@sunrise.vn>
2021-04-06 21:06:32 +0700

    hey

---
+++ environments/master/apps/bean/eks-cluster/variables.tf
@@ -0,0 +1,17 @@
+variable "cidr_block" {
+  type = string
+}
+
+variable "bounded_context" {
+  type = string
+}
+
+variable "app_type" {}
+variable "app_category" {}
+variable "app_name" {}
+variable "environment" {}
+variable "organisation" {}
+
+locals {
+    app_name = "${var.environment}-${var.app_type}-${var.app_category}-${var.app_name}"
+}

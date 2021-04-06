ea95c35965e5848bfa35dcf9ef3f3d4f556ab86c
Peter Bean <binh@sunrise.vn>
2021-04-06 07:55:22 +0700

    eks-proxy tf files

--- environments/master/apps/bean/bastion/files/terraform-init.sh
+++ environments/master/apps/bean/bastion/files/terraform-init.sh
@@ -1,7 +1,8 @@
 echo $1 > token.txt
 gh auth login --with-token < token.txt
-gh repo clone BeanTraining/terraform-infra && cd terraform-infra/ && git checkout dev-oregon && cd environments/master/apps/bean/eks-cluster
+gh repo clone BeanTraining/terraform-infra && cd terraform-infra/ && git checkout $7 && cd environments/master/apps/bean/eks-cluster
 printf "credentials \"app.terraform.io\" { \n token = \"$2\" \n }" > ~/.terraformrc
+printf "terraform { \n  required_version = \">= 0.12.0\" \n    backend \"remote\" { hostname = \"app.terraform.io\" organization = \"$6\" \n workspaces { name = \"$7-apps-bean-eks-cluster\" } } }" > backend.tf
 terraform init
 aws --profile default configure set aws_access_key_id "$3"
 aws --profile default configure set aws_secret_access_key "$4"

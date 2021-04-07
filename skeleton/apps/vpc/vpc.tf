8f31eccb1a5ac8440a84ed84148caf782eea8ec5
Peter Bean <binh@sunrise.vn>
2021-04-07 23:07:55 +0700

    VPC ?

--- skeleton/apps/vpc/vpc.tf
+++ skeleton/apps/vpc/vpc.tf
@@ -11,15 +11,15 @@ module "vpc" {
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
-  # public_subnet_tags = {
-  #   "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  #   "kubernetes.io/role/elb"                      = "1"
-  # }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
 
-  # private_subnet_tags = {
-  #   "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  #   "kubernetes.io/role/internal-elb"             = "1"
-  # }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
 }
 
 data "aws_availability_zones" "available" {

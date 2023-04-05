b74def7c6c4d517c3b41bbabb384ede0484bf6ac
hieuminh <minhhieu060799@gmail.com>
2023-04-05 17:16:24 +0700

    convert kops vpc id from data source -> var

--- platforms/simple-oregon/apps/vpc/cicd/main.tf
+++ platforms/simple-oregon/apps/vpc/cicd/main.tf
@@ -37,9 +37,9 @@ resource "aws_security_group" "allow_all" {
 }
 
 module "from_ptdev_cicd_to_ptdev_kops" {
-  source      = "git::ssh://git@github.com/BeanTraining/terraform-infra-skeleton.git//transit/vpc_peering?ref=ptdev"
-  peer_vpc_id = "vpc-014deb4e4d3877e44"
-  vpc_id      = "vpc-06e759b94f2c2eea0"
+  source      = "git::ssh://git@github.com/BeanTraining/terraform-infra-skeleton.git//transit/vpc_peering"
+  peer_vpc_id = data.aws_vpc.ptdev_kops.id
+  vpc_id      = module.skeleton.vpc_id
   providers = {
     aws.peer = aws
   }

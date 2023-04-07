693329bd4b5982483f58988e94000c855671a9b1
hieuminh <minhhieu060799@gmail.com>
2023-04-07 17:00:34 +0700

    add lambda creator

---
+++ platforms/simple-oregon/apps/lambda/nuke/main.tf
@@ -0,0 +1,34 @@
+module "iam" {
+  source = "git::ssh://git@github.com/HieuMinh67/terraform-infra-skeleton.git//services/iam_for_lambda?ref=ptdev"
+  env    = var.environment
+}
+
+module "lambda" {
+  source                = "git::ssh://git@github.com/HieuMinh67/terraform-infra-skeleton.git//services/lambda_function_service?ref=ptdev"
+  aws_region            = var.aws_region
+  aws_access_key_id     = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+  aws_account_id        = var.aws_account_ids[apps][lambda]
+  function_name         = "aws-nuke"
+  handler               = "aws-nuke"
+  # project = 
+  # bounded_context
+  # subnet_ids
+  # security_group_ids
+  # file_name
+  # lambda_logs_name
+  # is_in_vpc
+  # query_api_source_arn
+  # mutation_api_source_arn
+  # service_name
+  # aws_account_id
+  # organisation
+  # aws_region
+  # bean_region
+  # environment
+  # db_host
+  # db_user
+  # db_password
+  # db_name
+  # build_number
+}
---
+++ platforms/simple-oregon/apps/lambda/nuke/variables.tf
@@ -0,0 +1,8 @@
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "environment" {}
+variable "organisation" {}

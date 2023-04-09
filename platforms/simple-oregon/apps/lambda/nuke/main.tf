69d22d46ef938adaab548586e10790587511f487
Henry Pham <minhhieu060799@gmail.com>
2023-04-09 21:45:42 +0700

    add params for lambda

--- platforms/simple-oregon/apps/lambda/nuke/main.tf
+++ platforms/simple-oregon/apps/lambda/nuke/main.tf
@@ -6,13 +6,11 @@ module "iam" {
 module "lambda" {
   source                = "git::ssh://git@github.com/HieuMinh67/terraform-infra-skeleton.git//services/lambda_function_service?ref=ptdev"
   aws_region            = var.aws_region
-  aws_access_key_id     = var.aws_access_key_id
-  aws_secret_access_key = var.aws_secret_access_key
   aws_account_id        = var.aws_account_ids.apps.lambda
   function_name         = "aws-nuke"
   handler               = "aws-nuke"
-  # project = 
-  # bounded_context
+  project = "aws-nuke"
+  bounded_context = ""
   # subnet_ids
   # security_group_ids
   # file_name
@@ -20,15 +18,21 @@ module "lambda" {
   # is_in_vpc
   # query_api_source_arn
   # mutation_api_source_arn
-  # service_name
+  service_name = "aws-nuke"
   # aws_account_id
   # organisation
   # aws_region
   # bean_region
-  # environment
+  environment = "ptdev"
   # db_host
   # db_user
   # db_password
   # db_name
   # build_number
+  is_http_api = false
+  authorizer_id = ""
+  apigateway_id = ""
+  apigateway_execution_arn = ""
+  apigateway_name = ""
+  service_routes = null
 }

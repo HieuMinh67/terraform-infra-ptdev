48a24ac287faa332810c7e0413c4fdaa5ca89004
Minh-Hieu Pham <43398131+HieuMinh67@users.noreply.github.com>
2023-04-13 17:39:39 +0700

    init s3 module (#12)

---
+++ platforms/simple-oregon/apps/s3/bucket/main.tf
@@ -0,0 +1,5 @@
+module "s3_bucket" {
+  source = "git::ssh://git@github.com/BeanTraining/terraform-infra-skeleton.git//s3/bucket"
+  aws_account_id = var.aws_account_id
+  enable_versioning = var.enable_versioning
+}
---
+++ platforms/simple-oregon/apps/s3/bucket/variables.tf
@@ -0,0 +1,23 @@
+# mandatory vars from main ws
+variable "aws_account_id" {
+  type = string
+}
+variable "aws_account_ids" {}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+variable "tfe_token" {}
+variable "github_oauth_token" {}
+
+# child ws vars
+variable "aws_region" {
+  type = string
+  default = "us-west-2"
+}
+variable "suffix_name" {
+  type = string
+  default = "aws-lambda"
+}
+variable "enable_versioning" {
+  type = bool
+  default = false
+}

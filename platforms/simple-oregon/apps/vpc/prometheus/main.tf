02e7203c74ee25e1f95084301ff692ecb7eba90a
Minh-Hieu Pham <43398131+HieuMinh67@users.noreply.github.com>
2023-04-13 16:00:30 +0700

    fix skeleton VPC source (#11)

--- platforms/simple-oregon/apps/vpc/prometheus/main.tf
+++ platforms/simple-oregon/apps/vpc/prometheus/main.tf
@@ -1,5 +1,5 @@
 module "skeleton" {
-  source                     = "git::ssh://git@github.com/BeanTraining/terraform-infra-skeleton.git//apps/"
+  source                     = "git::ssh://git@github.com/BeanTraining/terraform-infra-skeleton.git//apps/vpc"
   vpc_name                   = "${var.app_name}-${var.app_category}-${var.app_type}-${var.platform}-${var.environment}"
   bounded_context            = "network"
   nat_instance_sg_id         = aws_security_group.allow_all.id
